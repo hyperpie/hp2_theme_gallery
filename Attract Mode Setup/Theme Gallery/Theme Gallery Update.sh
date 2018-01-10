6dfc1c2527eaee82f7ebef17928ea1cc33596d17
Mik McLean <haggistech@gmail.com>
2018-01-10 17:55:53 +0000

    test upload

--- Attract Mode Setup/Theme Gallery/Theme Gallery Update.sh
+++ Attract Mode Setup/Theme Gallery/Theme Gallery Update.sh
@@ -1,3 +1,4 @@
+rm -rf /home/pi/.attract/Attract\ Mode\ Setup/Theme\ Gallery/flyer
 mkdir -p /home/pi/.attract/themefiles
 git clone https://github.com/haggistech/hp2_theme_gallery.git "/home/pi/.attract/themefiles/"
 cp -r /home/pi/.attract/themefiles/* /home/pi/.attract/
