2a6d4bf3fa7e731988d1978dd89f61ec1014b89f
hyperpie <31572722+hyperpie@users.noreply.github.com>
2018-02-23 17:13:40 -0800

    Update test2.sh

--- Attract Mode Setup/Theme Gallery/test2.sh
+++ Attract Mode Setup/Theme Gallery/test2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
-
 echo_c()
 {
   w=$(stty size | cut -d" " -f2)       # width of the terminal
@@ -9,22 +7,9 @@ echo_c()
   printf "%"$((l+(w-l)/2))"s\n" "$1"   # print string padded to proper width (%Ws)
 }
 
-echo_c -n "Enter your name: "
-read name
-
 clear
-echo_c "Hello $name."
-echo_c "what is your favorite Color?"
-read color
-
-clear
-echo_c "$color is a goog color"
-echo_c "Now saving that info"
-echo_c "$name favorite color is $color."
-
-echo_c "Data saved."
 echo_c "please press Enter to Continue"
 read
 
 clear
-echo_c "Have a Good Day $name"
+echo_c "Have a Good Day"
