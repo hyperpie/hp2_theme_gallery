db613b0cf4b0d5a2933659dd4dbb381670556d1e
hyperpie <31572722+hyperpie@users.noreply.github.com>
2018-04-23 21:06:10 -0700

    Update Theme Gallery Update.sh

--- Attract Mode Setup/Theme Gallery/Theme Gallery Update.sh
+++ Attract Mode Setup/Theme Gallery/Theme Gallery Update.sh
@@ -7,6 +7,6 @@
 rm -rf /home/pi/.attract/Attract\ Mode\ Setup/Theme\ Gallery/flyers
 rm -rf /home/pi/.attract/Attract\ Mode\ Setup/Theme\ Gallery/*.sh
 mkdir -p /home/pi/.attract/themefiles
-git clone https://github.com/hyperpie/hp2_theme_gallery.git "/home/pi/.attract/themefiles/"
+git clone https://github.com/rpthemehub/HP2_Theme_Gallery.git"/home/pi/.attract/themefiles/"
 cp -r /home/pi/.attract/themefiles/* /home/pi/.attract/
 rm -rf /home/pi/.attract/themefiles
